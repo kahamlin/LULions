d5d0607e5e61472cc702d685bbea9ff2b269648c
TimAkor23 <timothy.akor@lions.lincoln.edu>
2025-11-07 18:08:40 -0500

    create react code

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>This is my first React app using npm and JavaScript.</p>
+    </div>
+  );
+}
+
+export default App;
